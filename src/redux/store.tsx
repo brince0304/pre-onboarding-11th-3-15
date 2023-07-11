8115e50dafcc0a7a868b01c9d9190a0f912fa476
brince0304 <brince0304@gmail.com>
2023-07-12 01:38:27 +0900

    feat : store 디스패치 내보내기 추가

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,12 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { issueReducer } from './issueReducer';
+import { useDispatch } from 'react-redux';
 
 const reducer = combineReducers({ issueReducer: issueReducer.reducer });
 
-export default configureStore({
+export type RootState = ReturnType<typeof reducer>;
+export type AppDispatch = typeof store.dispatch;
+
+export const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
