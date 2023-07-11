1364bfd566779b2d16fc67c5fbeb1e44785b8064
brince0304 <brince0304@gmail.com>
2023-07-12 01:39:48 +0900

    feat: index, app 라우터 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { IssueProvider } from './context/IssueContext';
 import { GithubService } from './services/githubService';
 import { HttpClient } from './client/httpClient';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import PrivateRouterProvider from './router/Router';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const baseUrl = 'https://api.github.com/';
+const baseUrl = 'https://api.github.com';
 const apiKey = process.env.REACT_APP_GITHUB_API_KEY as string;
 const owner = process.env.REACT_APP_GITHUB_OWNER as string;
 const repo = process.env.REACT_APP_GITHUB_REPO as string;
@@ -20,7 +20,7 @@ const issueService = new GithubService(httpClient, owner, repo);
 root.render(
   <IssueProvider issueService={issueService}>
     <Provider store={store}>
-      <App />
+      <PrivateRouterProvider />
     </Provider>
   </IssueProvider>,
 );
