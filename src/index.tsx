fc99da8ec571a05c5335299f31b00841dcc6a42b
brince0304 <brince0304@gmail.com>
2023-07-11 23:55:23 +0900

    feat: index.tsx 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { IssueProvider } from './context/IssueContext';
+import { GithubService } from './services/githubService';
+import { HttpClient } from './client/httpClient';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const baseUrl = 'https://api.github.com/';
+const apiKey = process.env.REACT_APP_GITHUB_API_KEY as string;
+const owner = process.env.REACT_APP_GITHUB_OWNER as string;
+const repo = process.env.REACT_APP_GITHUB_REPO as string;
+const httpClient = new HttpClient(baseUrl, apiKey);
+const issueService = new GithubService(httpClient, owner, repo);
 root.render(
-  <React.StrictMode>
+  <IssueProvider issueService={issueService}>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>
+</IssueProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
