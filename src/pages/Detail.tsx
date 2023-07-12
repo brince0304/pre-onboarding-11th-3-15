67231d2d8c0f2622676e997cbdecd6990923a6f1
brince0304 <brince0304@gmail.com>
2023-07-13 00:26:28 +0900

    fix: lint 제어구문 추가

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -22,6 +22,7 @@ const Detail = () => {
   useEffect(() => {
     fetch();
     handleScrollToTop();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleRefreshCallback = async () => {
     setIssue(undefined);
