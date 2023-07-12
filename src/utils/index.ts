4ac5f72cff975344ff3bfe5efc02deeb321c8354
brince0304 <brince0304@gmail.com>
2023-07-13 00:15:26 +0900

    feat: 날짜 포멧팅 함수 구현

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: string) => {
+  const newDate = new Date(date);
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const day = newDate.getDate();
+  return `${year}년 ${month}월 ${day}일`;
+};
