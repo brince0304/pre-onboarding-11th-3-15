9d486aafae081d9484d5d99014b1a494814b5db4
brince0304 <brince0304@gmail.com>
2023-07-13 00:17:35 +0900

    feat : IssueContext 로직 수정
    - 단일 조회 로직을 구현함에 따라 상황별로 필요한 비즈니스 로직을 구현

--- src/context/IssueContext.tsx
+++ src/context/IssueContext.tsx
@@ -1,45 +1,77 @@
-import { IIssue } from '../interfaces/IIssue';
+import { IIssues, IIssueChild } from '../interfaces/IIssues';
 import { createContext, ReactNode, useContext } from 'react';
 import { IGithubService } from '../services/githubService';
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '../redux/store';
-import { getIssues, reset, setError, setPage } from '../redux/issueReducer';
+import {
+  getIssueByIssueNumberAction,
+  getIssueByIssueNumberThunk,
+  getIssuesByPageThunk,
+  reset,
+  setError,
+} from '../redux/issueReducer';
 
 const IssueContext = createContext<IIssueContextReturn>({} as IIssueContextReturn);
 export const useIssues = () => useContext(IssueContext);
-
 const IssueProvider = ({ children, issueService }: { children: ReactNode; issueService: IGithubService }) => {
   const getIssuesByPage = issueService.getIssuesByPage.bind(issueService);
+  const getIssueByIssueNumber = issueService.getIssueByIssueNumber.bind(issueService);
   const issueState = useSelector((state: RootState) => state.issueReducer);
   const dispatch = useAppDispatch();
   const { loading, error, issues, page, hasMore } = issueState;
-  const handleUpPage = () => {
-    dispatch(setPage(page + 1));
-  };
+
   const handleResetPage = async () => {
     dispatch(reset());
-    await handleGetIssues();
+    await handleGetIssuesByPage();
   };
 
-  const handleGetIssues = async () => {
+  const handleGetIssuesByPage = async () => {
     try {
-      await dispatch(getIssues(getIssuesByPage)).unwrap();
+      await dispatch(getIssuesByPageThunk(getIssuesByPage)).unwrap();
     } catch (e) {
       dispatch(setError(e));
+    }
+  };
+  const handleDispatchGetIssueByIssueNumber = async (id: number) => {
+    try {
+      await dispatch(
+        getIssueByIssueNumberThunk({
+          getIssueByIssueNumber: getIssueByIssueNumber,
+          issueNumber: id,
+        }),
+      ).unwrap();
+    } catch (e) {
+      dispatch(setError(e));
+    }
+  };
+
+  const handleRefreshCurrentPost = async (id: number) => {
+    await handleDispatchGetIssueByIssueNumber(id);
+    return dispatch(getIssueByIssueNumberAction(id));
+  };
+
+  const handleGetIssueByIssueNumber = async (id: number) => {
+    const issue = await dispatch(getIssueByIssueNumberAction(id));
+    if (issue) {
+      return issue;
+    } else {
+      await handleDispatchGetIssueByIssueNumber(id);
+      return dispatch(getIssueByIssueNumberAction(id));
     }
   };
 
   return (
     <IssueContext.Provider
       value={{
-        handleGetIssues,
+        handleGetIssues: handleGetIssuesByPage,
         loading,
         error,
         issues,
         page,
         hasMore,
-        handleUpPage,
         handleResetPage,
+        handleGetIssueByIssueNumber: handleGetIssueByIssueNumber,
+        handleRefreshCurrentPost: handleRefreshCurrentPost,
       }}
     >
       {children}
@@ -49,13 +81,14 @@ const IssueProvider = ({ children, issueService }: { children: ReactNode; issueS
 
 interface IIssueContextReturn {
   handleGetIssues: () => void;
-  loading?: string;
-  error?: string | null;
-  issues?: IIssue;
-  page?: number;
-  hasMore?: boolean;
-  handleUpPage?: () => void;
-  handleResetPage?: () => void;
+  loading: string;
+  error: string | null;
+  issues: IIssues;
+  page: number;
+  hasMore: boolean;
+  handleResetPage: () => void;
+  handleGetIssueByIssueNumber: (id: number) => Promise<IIssueChild | undefined>;
+  handleRefreshCurrentPost: (id: number) => Promise<IIssueChild | undefined>;
 }
 
 export { IssueContext, IssueProvider };
